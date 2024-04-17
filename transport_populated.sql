6fa2f870c612a2616b229c8eaff684f427fad1ab
ridwana-ridee <122758483+ridwana-ridee@users.noreply.github.com>
2024-04-17 11:25:17 -0400

    Add files via upload

---
+++ transport_populated.sql
@@ -0,0 +1,7 @@
+USE `world-cup-db`;
+
+INSERT INTO transportation (transport_type, location, availability, venue_id) VALUES
+('Shuttle Service', 'Nearby Hotel Parking Lot', 'Available', 1),
+('Public Transit', 'City Center Bus Terminal', 'Limited', 2),
+('Parking Facility', 'Stadium Parking Lot', 'Available', 3),
+('Ride-Sharing', 'Designated Drop-off Zone', 'Available', 4);
