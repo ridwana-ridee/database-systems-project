7e2ee44c7ceb65da2a95da8b248e3b4663be4cea
ridwana-ridee <122758483+ridwana-ridee@users.noreply.github.com>
2024-04-17 11:19:02 -0400

    Add files via upload

---
+++ ticket_populated.sql
@@ -0,0 +1,7 @@
+USE `world-cup-db`;
+
+INSERT INTO ticket (ticket_type, price, ticket_status, match_id) VALUES
+('Standard', 50.00, 'Available', 1),
+('VIP', 150.00, 'Available', 2),
+('General Admission', 30.00, 'Sold Out', 3),
+('Student Discount', 25.00, 'Available', 4);
