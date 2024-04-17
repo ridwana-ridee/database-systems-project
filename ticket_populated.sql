76d5995cdd24d67f151c4cffca3900bb1cab90f4
MalSyed <165718602+MalSyed@users.noreply.github.com>
2024-04-17 11:22:30 -0400

    Update ticket_populated.sql

--- ticket_populated.sql
+++ ticket_populated.sql
@@ -1,6 +1,6 @@
 USE `world-cup-db`;
 
-INSERT INTO ticket (ticket_type, price, ticket_status, match_id) VALUES
+INSERT INTO ticket (ticket_type, price, ticket_status, game_id) VALUES
 ('Standard', 50.00, 'Available', 1),
 ('VIP', 150.00, 'Available', 2),
 ('General Admission', 30.00, 'Sold Out', 3),
