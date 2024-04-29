94c2b167c44ee633e9a973af50e157a4b3e9ab30
ridwana-ridee <122758483+ridwana-ridee@users.noreply.github.com>
2024-04-29 02:27:22 -0400

    Delete user_populated.sql

--- user_populated.sql
+++
@@ -1,7 +0,0 @@
-USE `world-cup-db`;
-
-INSERT INTO user (username, pass_word, user_role, fName, lName)
-VALUES ('ridee2002', 'ridwana2002', 'admin', 'Ridwana', 'Ridee'),
-('syed2002', 'malik2002', 'admin', 'Malik', 'Syed'),
-('inalkac2000', 'zeynep2000', 'admin', 'Zeynep', 'Inalkac'),
-('demoUser', 'demo1234', 'user', 'Demo', 'User');
