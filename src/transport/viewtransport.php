88d918b634b5d183cc8c71fe704db2e385fbca6c
ridwana-ridee <122758483+ridwana-ridee@users.noreply.github.com>
2024-04-29 04:46:09 -0400

    Update viewtransport.php

--- src/transport/viewtransport.php
+++ src/transport/viewtransport.php
@@ -5,16 +5,8 @@
 </head>
 <body>
     <?php
-        $host = "localhost";
-        $user = "root";
-        $pass = "";
-        $dbname = "world-cup-db";
+        require 'db_config.php';
 
-        $conn = new mysqli($host, $user, $pass, $dbname);
-
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
         $sql = "SELECT * FROM transportation";
         
         $result = $conn->query($sql);
